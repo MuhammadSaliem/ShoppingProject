0d8eabf8c4a2030d4150da834c969583ea635947
_Saleem_ <mohamedselim701@gmail.com>
2022-08-02 14:49:57 +0200

    Create base components

---
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'app-shopping-list-edit',
+    templateUrl: 'shopping-list-edit.component.html',
+    styles: ['.p {size: 50px}']
+
+})
+
+
+export class ShoppingListEditComponent {
+
+}
